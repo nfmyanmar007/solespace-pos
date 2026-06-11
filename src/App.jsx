1f0687d95cd0da21998aec4125c55910e50e9f75
nfmyanmar007 <nfmyanmar007@gmail.com>
2026-06-11 12:18:36 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useSession } from './context/SessionContext'
 import LoginPage from './pages/LoginPage'
 import POSHome from './pages/POSHome'
 import PaymentPage from './pages/PaymentPage'
+import ReceiptPage from './pages/ReceiptPage'
+import SummaryPage from './pages/SummaryPage'
 
 function ProtectedRoute({ children }) {
   const { session } = useSession()
@@ -20,6 +22,12 @@ export default function App() {
       } />
       <Route path="/pos/payment" element={
         <ProtectedRoute><PaymentPage /></ProtectedRoute>
+      } />
+      <Route path="/pos/receipt/:txnId" element={
+        <ProtectedRoute><ReceiptPage /></ProtectedRoute>
+      } />
+      <Route path="/pos/summary" element={
+        <ProtectedRoute><SummaryPage /></ProtectedRoute>
       } />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>

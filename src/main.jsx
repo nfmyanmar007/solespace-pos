d5719efa4bfa8d3052a62b23a8b67bb6a3dcb9a4
nfmyanmar007 <nfmyanmar007@gmail.com>
2026-06-11 10:54:54 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { SessionProvider } from './context/SessionContext.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <SessionProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </SessionProvider>
     </BrowserRouter>
   </React.StrictMode>,
